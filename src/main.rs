f2e592d02041cded550e7b56b64573d9ee4b57a3
Gexon <alimgan@gmail.com>
2015-09-14 02:08:51 +0500

    уроки 1

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,12 @@
+static MAX_HEALF: i32 = 100;
+static GAME_NAME: &'static str = "Убийца доты!";
+
 fn main() {
     dota_mast_die();
 }
 
+
+// комент, тестовая функция.
 fn dota_mast_die(){
     println!("Dota mast die!");
 }

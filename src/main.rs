8bd87358ffb72ee3d452c04c6f1fd66131d53924
Gexon <alimgan@gmail.com>
2015-09-14 22:03:37 +0500

    уроки 1

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,20 @@
-
+use std::io;
 
 fn main() {
+		input_number();
+
+
 }
 
+fn input_number() {
+	println!("Угадай загаданное мной число");
+	println!("Введите число");
 
+	let mut number = String::new();
 
-#[test]
-fn main() {
+					io::stdin().read_line(&mut number)
+			.ok()
+		.expect("ошибка ввода какая-то");
 
+	println!("Ты угадал! {}", number);
 }

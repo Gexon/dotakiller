bf04bb7a1ce78a638175e4dae3b87ac86f030646
Gexon <alimgan@gmail.com>
2015-09-18 22:27:18 +0500

    уроки 1

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,68 @@
-use std::io;
+extern crate rand;
+
+use std::thread;
+use std::sync::{Mutex, Arc};
+use rand::Rng;
 
 fn main() {
-		input_number();
+	let table = Arc::new(Table { forks: vec![
+	Mutex::new(()),
+	Mutex::new(()),
+	Mutex::new(()),
+	Mutex::new(()),
+	Mutex::new(()),
+	Mutex::new(()),
+	]
+	});
 
+	let philosophers = vec![
+	Philosopher::new("Markizz", 0, 1),
+	Philosopher::new("c0deum", 1, 2),
+	Philosopher::new("GoodGod", 2, 3),
+	Philosopher::new("hi4C0CK", 3, 4),
+	Philosopher::new("0sk0L0k", 4, 5),
+	Philosopher::new("a1983", 5, 0),
+	];
 
+	let handles: Vec<_> = philosophers.into_iter().map(|p| {
+			let table = table.clone();
+
+			thread::spawn(move || {
+				p.eat(&table);
+		})
+	}).collect();
+
+	for h in handles {
+				h.join().unwrap();
+	}
 }
 
-fn input_number() {
-	println!("Угадай загаданное мной число");
-	println!("Введите число");
+struct Philosopher {
+name: String,
+left: usize,
+right: usize,
+}
 
-	let mut number = String::new();
+struct Table {
+forks: Vec<Mutex<()>>,
+}
 
-					io::stdin().read_line(&mut number)
-			.ok()
-		.expect("ошибка ввода какая-то");
+impl Philosopher {
+fn new(name: &str, left: usize, right: usize) -> Philosopher {
+	Philosopher{
+	name: name.to_string(),
+	left: left,
+	right: right,
+	}
+}
 
-	println!("Ты угадал! {}", number);
+fn eat(& self, table: &Table){
+	let _left = table.forks[self.left].lock().unwrap();
+	let _right = table.forks[self.right].lock().unwrap();
+
+	println!("{} приступил к питанию", self .name);
+	let eat_time = rand::thread_rng().gen_range(500, 2000);
+	thread::sleep_ms(eat_time);
+	println!("{} кончил", self .name);
+}
 }

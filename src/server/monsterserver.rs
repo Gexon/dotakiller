ec307fab0430ed3fa7955c80af545d01366dbe0d
Gexon <alimgan@gmail.com>
2016-11-20 10:34:36 +0500

    подготовка к приему монстер-сервера. компилится.

--- src/server/monsterserver.rs
+++ src/server/monsterserver.rs
@@ -6,6 +6,9 @@ use std::net::{TcpStream, TcpListener};
 use std::io::{BufReader, BufWriter};
 use std::io::prelude::*;
 use std::time::Duration;
+use std::str;
+
+use byteorder::{ByteOrder, BigEndian};
 
 use bincode::SizeLimit;
 use bincode::rustc_serialize::{encode, decode};
@@ -71,7 +74,9 @@ impl System for MonsterServerSystem {
     }
 
     fn process_all(&mut self, entities: &mut Vec<&mut Entity>, _world: &mut WorldHandle, _data: &mut DataList) {
+        println!("Готовимся принять данные от Монстра-сервера.");
         let monster_export: MonsterExport = self.server_data.read();
+        println!("Данные от Монстра-сервера получены.");
         println!("Приняли монстра {}", monster_export.id);
         for entity in entities {
             entity.remove_component::<MonsterServerClass>();
@@ -104,11 +109,48 @@ impl MonsterServer {
     }
 
     fn read(&mut self) -> MonsterExport {
-        //fn read(&mut self, buf: &mut [u8]) -> io::Result<usize>
-        let mut buf = vec![];
+        // создаем читателя
         let mut reader = BufReader::new(&self.stream);
-        reader.read(&mut buf).unwrap();
-
-        decode(&buf[..]).unwrap()
+        // готовим вектор для примема размера входящих данных
+        let mut buf_len = [0u8; 8];
+        // принимаем данные
+        let bytes = match reader.read(&mut buf_len) {
+            Ok(n) => {
+                let s = str::from_utf8(&buf_len[..]).unwrap();
+                println!("Содержимое сообщения о длине входящих данных:{}, количество считанных байт:{}", s, n);
+                n
+            },
+            Err(_) => {
+                0
+            }
+        };
+        if bytes < 8 {
+            warn!("Ошибка. Сообщение о длине входящих данных меньше 8 байт и равно: {} bytes", bytes);
+            println!("Ошибка. Сообщение о длине входящих данных меньше 8 байт и равно: {} bytes", bytes);
+        }
+        // превращаем в нормальный вид длину входящих данных.
+        let msg_len = BigEndian::read_u64(buf_len.as_ref());
+        let msg_len = msg_len as usize;
+        debug!("Ожидаемая длина сообщения {}", msg_len);
+        println!("Ожидаемая длина сообщения {}", msg_len);
+        // подготавливаем вектор для принимаемых данных.
+        let mut recv_buf: Vec<u8> = Vec::with_capacity(msg_len);
+        unsafe { recv_buf.set_len(msg_len); }
+        //let stream_ref = <TcpStream as Read>::by_ref(&self.stream);
+        //match self.stream.take(msg_len as u64).read(&mut recv_buf) {
+        // прием данных
+        match reader.read(&mut buf_len) {
+            Ok(n) => {
+                debug!("CONN : считано {} байт", n);
+                println!("CONN : считано {} байт", n);
+                if n < msg_len as usize {
+                    println!("Не осилил достаточно байт");
+                }
+                decode(&recv_buf[..]).unwrap()
+            }
+            Err(_) => {
+                panic!("Неудалось считать буфер для сокета");
+            }
+        }
     }
 }

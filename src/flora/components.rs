daa074804c2abc1779bf422c0d282cdd039963b7
Gexon <alimgan@gmail.com>
2016-11-24 10:04:36 +0500

    Монстер-сервер успешно подключен. компилится.

--- src/flora/components.rs
+++ src/flora/components.rs
@@ -1,19 +1,25 @@
 // тут будут расписаны все компоненты специфичные только для растений.
 
+
 use tinyecs::*;
 use time::PreciseTime;
 
-// метка принадлежности к классу арстений.
+
+/// метка принадлежности к классу арстений.
 pub struct FloraClass;
 
 impl Component for FloraClass {}
 
+
+/// уникальный номер растения
 pub struct IdHerb {
     pub id: i64,
 }
 
 impl Component for IdHerb {}
 
+
+/// информация о состоянии растения
 pub struct FloraState {
     pub state: i32,
     pub growth_time: PreciseTime,

8590a4776069b7917f92fcda4e879c412a4d998b
Gexon <alimgan@gmail.com>
2017-03-18 17:19:08 +0500

    Продолжаем работу над внедрением деревьев поведений. не компилиться вроде.

--- src/utility/enums.rs
+++ src/utility/enums.rs
@@ -67,18 +67,86 @@ pub enum BehaviorEventEnum {
 }
 
 
-///// Вынимаем перечисление по индексу.
-//pub fn get_behavior_state_enum(idx: u32) -> BehaviorStateEnum {
-//    match idx {
-//        0 => BehaviorStateEnum::Init,
-//        1 => BehaviorStateEnum::Sleep,
-//        2 => BehaviorStateEnum::Walk,
-//        3 => BehaviorStateEnum::FindFood,
-//        4 => BehaviorStateEnum::FindWater,
-//        5 => BehaviorStateEnum::Meal,
-//        6 => BehaviorStateEnum::WaterIntake,
-//        7 => BehaviorStateEnum::MoveToTarget,
-//        8 => BehaviorStateEnum::CheckMoveToTarget,
-//        _ => BehaviorStateEnum::Init,
-//    }
-//}
+
+/// Список узлов графа
+pub enum _NodeType {
+    //функция, которая должна выполниться при посещении данного узла.
+    Action,
+    //чтобы определить, выполнять или нет следующие за ним узлы
+    Condition,
+    //последовательность, до первого узла Fail, либо выполняет все и возвращает Success
+    Sequencer,
+    //до первого узла возвращающего Success
+    Selector,
+    //роль цикла for
+    Iterator,
+    // пока не использую
+    Parallel,
+}
+
+/// Описание поведения. https://github.com/PistonDevelopers/ai_behavior/blob/master/src/behavior.rs
+#[derive(Clone, RustcDecodable, RustcEncodable, PartialEq)]
+pub enum Behavior<A> {
+    /// Описания высоко-уровеневых действий.
+    Action(A),
+    /// Преобразует `Success` в `Failure` и наоборот.
+    //Fail(Box<Behavior<A>>), // надо ли?
+    /// Игнорирует неудачи и возвращает "Success".
+    //AlwaysSucceed(Box<Behavior<A>>), // надо ли?
+    /// Выполняет последовательность, пока не встретит "Success".
+    /// Завершается неудачей, нет ни одного "Success".
+    Select(Vec<Behavior<A>>),
+    /// `If(condition, success, failure)`
+    If(Box<Behavior<A>>, Box<Behavior<A>>, Box<Behavior<A>>),
+    /// Выполняет последовательность, пока все "Success".
+    /// Последовательность прерывается и возвращает сбой, если встречается сбой.
+    /// The sequence succeeds if all the behavior succeeds.
+    /// Последовательность возвращает "Success", если все элементы отработали без сбоев.
+    Sequence(Vec<Behavior<A>>),
+    /// Loops while conditional behavior is running.
+    /// Выполняется цикл в то время как "условное" поведение работает.
+    ///
+    /// Succeeds if the conditional behavior succeeds.
+    /// Выполняется, если условное поведение преуспевает.
+    /// Fails if the conditional behavior fails,
+    /// or if any behavior in the loop body fails.
+    /// Вернет сбой, если условное поведение не удается,
+    /// или если какое-либо поведение в тело цикла не выполняется.
+    While(Box<Behavior<A>>, Vec<Behavior<A>>),
+    /// Runs all behaviors in parallel until all succeeded.
+    /// Работает все поведение параллельно, пока все получилось.
+    ///
+    /// Succeeds if all behaviors succeed.
+    /// Успех, если все элементы успешны.
+    /// Fails is any behavior fails.
+    /// Ошибка, если хотябы один вернет ошибку.
+    //WhenAll(Vec<Behavior<A>>), // надо ли?
+    /// Runs all behaviors in parallel until one succeeds.
+    /// Работает все поведение параллельно, пока хотябы одно поведение удачно.
+    ///
+    /// Succeeds if one behavior succeeds.
+    /// В том случае, если одно поведение преуспевает.
+    /// Fails if all behaviors failed.
+    /// Вернет сбой, если все элемент ы вернут сбой.
+    //WhenAny(Vec<Behavior<A>>), // надо ли?
+    /// Runs all behaviors in parallel until all succeeds in sequence.
+    /// Работает все поведение параллельно, пока все удается в определенной последовательности.
+    ///
+    /// Succeeds if all behaviors succeed, but only if succeeding in sequence.
+    /// В том случае, если все варианты поведения, удается, но только если успех в определенной последовательности.
+    /// Fails if one behavior fails.
+    /// Вернет неудачу, если хотябы в одном элементе происходит сбой.
+    After(Vec<Behavior<A>>), // надо ли?
+}
+
+
+/// The result of a behavior or action.
+#[derive(Copy, Clone, RustcDecodable, RustcEncodable, PartialEq, Eq, Debug)]
+pub enum Status {
+    /// The behavior or action succeeded.
+    Success,
+    /// The behavior or action failed.
+    Failure,
+    /// The behavior or action is still running.
+    Running,
+}
